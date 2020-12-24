de7ca98ee4a014d94dc875db887e4a37d6b36245
muhammadadamfirdaus <adam@muhammadadamfirdaus>
2020-12-24 23:54:33 +0700

    mobile view

--- frontend/src/views/About.js
+++ frontend/src/views/About.js
@@ -5,44 +5,47 @@ import Header from "../components/Header";
 const About = () => {
 	return (
 		<>
-			<Header />
-			<section className="col intro">
-				<div className="wrapper">
-					<h1>About Me</h1>
-					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam quos
-						magnam dolorum consequuntur quo sint quia beatae quasi distinctio
-						aliquam iusto maxime, ad quas nemo non eaque corrupti explicabo
-						doloribus amet. Eveniet, libero sed, tempora atque architecto
-						deserunt praesentium vero aspernatur ex laboriosam impedit a commodi
-						officiis, deleniti unde tempore. Aliquam vel enim nemo itaque amet,
-						hic voluptatum esse cum accusantium corporis, dolorum exercitationem
-						omnis! Quasi, perspiciatis optio veniam perferendis quos harum rerum
-						natus earum quisquam delectus, consequuntur, neque praesentium!
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
-						architecto minima quia ratione ea numquam iste id error. Minima
-						provident sunt ratione facere quasi exercitationem, illo neque
-						facilis commodi. Ipsa, rem eum quis at consequatur similique
-						accusamus animi maiores, adipisci veritatis recusandae quasi. Quas
-						ex nobis alias quam nemo placeat ad id expedita nihil! Quam aliquid
-						maiores soluta reiciendis animi expedita. Voluptas beatae amet
-						molestiae voluptatum deserunt eligendi, nemo saepe, a at ea suscipit
-						ullam consectetur dolore nesciunt quas consequatur error ipsa iure
-						odit numquam. Eius, dolore enim dolorem hic rem, ad repudiandae
-						minus blanditiis similique fugit distinctio possimus.
-					</p>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
-						saepe facilis quia id reprehenderit accusantium minima non nihil
-						perspiciatis rerum fuga, debitis aliquid molestiae illum qui unde
-						nesciunt eveniet beatae eos, veritatis odio vitae. Necessitatibus
-						nostrum saepe debitis repudiandae atque. Expedita, impedit numquam
-						unde quibusdam beatae eligendi rem qui fugit.
-					</p>
-				</div>
-			</section>
+			<div className="about">
+				<Header />
+				<section className="col intro">
+					<div className="wrapper">
+						<h1>About Me</h1>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+							quos magnam dolorum consequuntur quo sint quia beatae quasi
+							distinctio aliquam iusto maxime, ad quas nemo non eaque corrupti
+							explicabo doloribus amet. Eveniet, libero sed, tempora atque
+							architecto deserunt praesentium vero aspernatur ex laboriosam
+							impedit a commodi officiis, deleniti unde tempore. Aliquam vel
+							enim nemo itaque amet, hic voluptatum esse cum accusantium
+							corporis, dolorum exercitationem omnis! Quasi, perspiciatis optio
+							veniam perferendis quos harum rerum natus earum quisquam delectus,
+							consequuntur, neque praesentium!
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
+							architecto minima quia ratione ea numquam iste id error. Minima
+							provident sunt ratione facere quasi exercitationem, illo neque
+							facilis commodi. Ipsa, rem eum quis at consequatur similique
+							accusamus animi maiores, adipisci veritatis recusandae quasi. Quas
+							ex nobis alias quam nemo placeat ad id expedita nihil! Quam
+							aliquid maiores soluta reiciendis animi expedita. Voluptas beatae
+							amet molestiae voluptatum deserunt eligendi, nemo saepe, a at ea
+							suscipit ullam consectetur dolore nesciunt quas consequatur error
+							ipsa iure odit numquam. Eius, dolore enim dolorem hic rem, ad
+							repudiandae minus blanditiis similique fugit distinctio possimus.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+							saepe facilis quia id reprehenderit accusantium minima non nihil
+							perspiciatis rerum fuga, debitis aliquid molestiae illum qui unde
+							nesciunt eveniet beatae eos, veritatis odio vitae. Necessitatibus
+							nostrum saepe debitis repudiandae atque. Expedita, impedit numquam
+							unde quibusdam beatae eligendi rem qui fugit.
+						</p>
+					</div>
+				</section>
+			</div>
 		</>
 	);
 };
