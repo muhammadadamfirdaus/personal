d0ccd4b3bc96a06b08394d65bb6eaf2be2c2374e
muhammadadamfirdaus <adam@muhammadadamfirdaus.com>
2021-01-01 16:32:40 +0700

    creating root class

--- frontend/src/views/Blog.js
+++ frontend/src/views/Blog.js
@@ -9,36 +9,42 @@ const recentPosts = posts.sort((a, b) => a.time_published - b.time_published);
 const Blog = () => {
 	return (
 		<>
-			<Header />
-			<div className="content blog col col-2">
-				<div className="wrapper">
-					<ul className="list">
-						{recentPosts.map((x) => (
-							<li key={x.id}>
-								<h3>
-									<Link
-										to={`/blog/${x.id}/${x.title
-											.replace(/[\W_]+/g, "-")
-											.toLowerCase()}`}
-									>
-										{x.title}
-									</Link>
-								</h3>
-								<span>
-									{x.time_published.slice(0, 10).split("-").reverse().join("-")}
-								</span>
-								<p>{x.description}</p>
-							</li>
-						))}
-					</ul>
-					<aside>
-						<div className="widget">
-							<div className="wrapper"></div>
-						</div>
-					</aside>
+			<div className="App blog">
+				<Header />
+				<div className="content col col-2">
+					<div className="wrapper">
+						<ul className="list">
+							{recentPosts.map((x) => (
+								<li key={x.id}>
+									<h3>
+										<Link
+											to={`/blog/${x.id}/${x.title
+												.replace(/[\W_]+/g, "-")
+												.toLowerCase()}`}
+										>
+											{x.title}
+										</Link>
+									</h3>
+									<span>
+										{x.time_published
+											.slice(0, 10)
+											.split("-")
+											.reverse()
+											.join("-")}
+									</span>
+									<p>{x.description}</p>
+								</li>
+							))}
+						</ul>
+						<aside>
+							<div className="widget">
+								<div className="wrapper"></div>
+							</div>
+						</aside>
+					</div>
 				</div>
+				<Footer />
 			</div>
-			<Footer />
 		</>
 	);
 };
--- frontend/src/views/BlogDetailPost.js
+++ frontend/src/views/BlogDetailPost.js
@@ -5,33 +5,34 @@ import posts from "../data/posts";
 
 const BlogDetailPost = ({ match }) => {
 	const detailPost = posts.find((p) => p.id === match.params.id);
-	console.log(detailPost);
 	return (
 		<>
-			<Header />
-			<div className="content blog detail">
-				<div className="wrapper">
-					<div>
-						<h2>{detailPost.title}</h2>
-						<span>
-							{detailPost.time_published
-								.slice(0, 10)
-								.split("-")
-								.reverse()
-								.join("-")}
-						</span>
-						<article>
-							<p>{detailPost.body}</p>
-						</article>
-					</div>
-					<aside>
-						<div className="widget">
-							<div className="wrapper"></div>
+			<div className="App blog detail">
+				<Header />
+				<div className="content">
+					<div className="wrapper">
+						<div>
+							<h2>{detailPost.title}</h2>
+							<span>
+								{detailPost.time_published
+									.slice(0, 10)
+									.split("-")
+									.reverse()
+									.join("-")}
+							</span>
+							<article>
+								<p>{detailPost.body}</p>
+							</article>
 						</div>
-					</aside>
+						<aside>
+							<div className="widget">
+								<div className="wrapper"></div>
+							</div>
+						</aside>
+					</div>
 				</div>
+				<Footer />
 			</div>
-			<Footer />
 		</>
 	);
 };
