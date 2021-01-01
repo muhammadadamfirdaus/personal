25df20ffd62e51edffb665d20161936518b593fe
muhammadadamfirdaus <adam@muhammadadamfirdaus.com>
2021-01-01 18:41:35 +0700

    rerender blog post

--- frontend/src/views/BlogDetailPost.js
+++ frontend/src/views/BlogDetailPost.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import posts from "../data/posts";
@@ -9,21 +10,32 @@ const BlogDetailPost = ({ match }) => {
 		<>
 			<div className="App blog detail">
 				<Header />
-				<div className="content">
+				<div className="content col col-3">
 					<div className="wrapper">
 						<div>
+							<Link to="/blog/">
+								<span>Kembali</span>
+							</Link>
 							<h2>{detailPost.title}</h2>
 							<span>
+								Ditulis pada:{" "}
 								{detailPost.time_published
 									.slice(0, 10)
 									.split("-")
 									.reverse()
 									.join("-")}
+								, pukul {detailPost.time_published.slice(14, 19)} WIB.
 							</span>
-							<article>
-								<p>{detailPost.body}</p>
-							</article>
+							<span>Oleh: {detailPost.author}</span>
+							<article
+								dangerouslySetInnerHTML={{ __html: detailPost.body }}
+							></article>
 						</div>
+						<aside>
+							<div className="widget">
+								<div className="wrapper">x</div>
+							</div>
+						</aside>
 						<aside>
 							<div className="widget">
 								<div className="wrapper"></div>
